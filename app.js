152959d97c2afd88d14421b7d66895c457e5e761
Dennis <smilleew9621@gmail.com>
2023-06-29 14:12:52 +0800

    feat: add generate password page

--- app.js
+++ app.js
@@ -8,14 +8,25 @@ app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
+app.use(express.json()); // Used to parse JSON bodies
+app.use(express.urlencoded()); // Parse URL-encoded bodies using query-string library
+// or
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies using qs library
+
+//使用靜態資料
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.redirect('/passwordGenerator')
 })
 
 //產生密碼route
 app.get('/passwordGenerator', (req, res) => {
+  //console.log(req.body)
   res.render('index')
 })
+
+
 
 //啟動監聽
 app.listen(port, () => {
