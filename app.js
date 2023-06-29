2e87db1aab253c55d9d694df608b1420ee7d8ade
Dennis <smilleew9621@gmail.com>
2023-06-29 11:18:10 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+
+//
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', './views')
 
 app.get('/', (req, res) => {
   res.redirect('/passwordGenerator')
@@ -8,9 +14,10 @@ app.get('/', (req, res) => {
 
 //產生密碼route
 app.get('/passwordGenerator', (req, res) => {
-  res.send('generate password')
+  res.render('index')
 })
 
+//啟動監聽
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
