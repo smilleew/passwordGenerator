93a6d84dd9512a45435d2539f58e7e4a6140412a
Dennis <smilleew9621@gmail.com>
2023-06-29 16:44:48 +0800

    feat: update generate password function ranfom length password

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,34 @@
+const lower = 'abcdefghijklmnopqrstuvwxyz'
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const number = '0123456789'
+const symbol = '`~!@$%^&*()-={}[]|;:"<>,.?/'
+
 const showinfo = function (length, lowercase, uppercase, numbers, symbols, excludeCharacters) {
-  return length + lowercase + uppercase + numbers + symbols + excludeCharacters;
+  let collection = []
+  let newpassword = ''
+
+  if (lowercase === 'on') {
+    collection = collection.concat(...lower)
+  } 
+
+  if (uppercase === 'on') {
+    collection = collection.concat(...upper)
+  }  
+
+  if (numbers === 'on') {
+    collection = collection.concat(...number)
+  }  
+
+  if (symbols === 'on') {
+    collection = collection.concat(...symbol)
+  }
+
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * collection.length)
+    newpassword += collection[index]
+  }
+
+  return newpassword
 }
 
 
