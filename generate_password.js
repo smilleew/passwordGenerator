d4dc44b3260bfb8944457086f4754d7634b8d20e
Dennis <smilleew9621@gmail.com>
2023-06-30 07:54:23 +0800

    feat: add no valid situation

--- generate_password.js
+++ generate_password.js
@@ -4,9 +4,12 @@ const number = '0123456789'
 const symbol = '`~!@$%^&*()-={}[]|;:"<>,.?/'
 
 const showinfo = function (formValues) {
+  //隨機抽選項目
   let collection = []
+  //新密碼
   let newpassword = ''
 
+  //如果checkbox有勾選就將相關的資料放入抽選項目中
   if (formValues.lowercaseCharacters === 'on') {
     collection = collection.concat(...lower)
   } 
@@ -23,6 +26,7 @@ const showinfo = function (formValues) {
     collection = collection.concat(...symbol)
   }
 
+  //刪除不想要的項目
   if (formValues.excludeCharacters) {
     collection = collection.filter(character => {
       if (formValues.excludeCharacters.includes(character)) {
@@ -32,6 +36,12 @@ const showinfo = function (formValues) {
     })
   }
 
+  //如果沒有隨機項目不產生密碼
+  if (collection.length === 0) {
+    return 'There is no valid character in your selection.'
+  }
+
+  //依照數量隨機產生密碼
   for (let i = 0; i < formValues.passwordLength; i++) {
     const index = Math.floor(Math.random() * collection.length)
     newpassword += collection[index]
